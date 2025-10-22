c3e92574607cb5226ad5913adbef31bf2f25c449
Realbhi <abhishekhiregouda160@gmail.com>
2025-10-22 11:32:10 +0530

    Function concept

---
+++ file03- Function/Functions.go
@@ -0,0 +1,46 @@
+// declartion of  - parameters and result type
+
+/*
+package main
+
+import (
+	// "errors"
+	"fmt"
+	"log"
+	"strings"
+)
+
+func add(x , y int) (sum,sub int, wrong error) {
+	sum = x+y;
+	sub = x-y
+	fmt.Println(sum,sub)
+	return sum ,sub, nil
+}
+
+// different ways to declare functions.
+// funcsub(x,yint)(zint) {z=x-y;return}
+//  funcsub (int,int) (int) { return 0}  : this is a invalid type of declaration
+
+
+// signature 
+
+
+// error handling : fucntion like Strings.Fields() and strconv.Atoi(), errors package , error interface
+func main(){
+	// Example usage of add function
+	sum, sub,err := add(10, 5)
+	var name string ="10 20"
+	var slicething []string = strings.Fields(name)
+	if slicething[1] == "" {
+		log.Fatalf("Expecting second element in slice %v",err)
+	}
+	if err !=nil {
+        fmt.Errorf ("Error in add function: %v",err)
+	}
+	fmt.Println("Sum:", sum, "Subtraction:", sub)
+}
+
+// in go programming we have mechanism of exception like other language but only of the bugs. 
+// Routine error is for normals programs.
+
+*/
